edaa1f46860e477b3ae3c0a2c062c7b7f8000c69
k-gnusarev <kgnusaryov@yandex.ru>
2021-04-14 11:21:48 +0400

    init

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 const { PORT = 3000 } = process.env;
 
